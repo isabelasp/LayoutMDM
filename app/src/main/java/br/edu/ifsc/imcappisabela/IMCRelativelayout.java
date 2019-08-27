28eefb909ba6de35e686368dc60a4cacc31c4912
ThaisKuminek <thaiseduarda_22@outlook.com>
2019-08-26 22:19:08 -0300

    Exercico 3

--- app/src/main/java/br/edu/ifsc/imcappisabela/IMCRelativelayout.java
+++ app/src/main/java/br/edu/ifsc/imcappisabela/IMCRelativelayout.java
@@ -4,26 +4,36 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class IMCRelativelayout extends AppCompatActivity {
-
+    EditText edaltura;
+    EditText edpeso;
+    TextView result;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imcrelativelayout);
+        EditText edaltura = findViewById(R.id.edAltura);
+        EditText edpeso = findViewById(R.id.edPeso);
+        TextView result = findViewById(R.id.result);
+        button = findViewById(R.id.button11);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 
     public void onCalcularIMC(View view){
 
-        EditText edaltura = findViewById(R.id.edAltura);
-        EditText edpeso = findViewById(R.id.edPeso);
-
         double altura = Double.parseDouble(edaltura.getText().toString());
         double peso = Double.parseDouble(edpeso.getText().toString());
 
-        TextView result = findViewById(R.id.result);
         double IMC = peso/(altura*altura);
         result.setText("Resultado "+IMC);
 
